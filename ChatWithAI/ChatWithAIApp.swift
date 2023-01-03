009c84672f38947c8666dd20025a6a1a49d59a96
Huy Ong <huy.ong@sjsu.edu>
2023-01-02 16:31:00 -0800

    Initial Commit

---
+++ ChatWithAI/ChatWithAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatWithAIApp.swift
+//  ChatWithAI
+//
+//  Created by Huy Ong on 1/2/23.
+//
+
+import SwiftUI
+
+@main
+struct ChatWithAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
